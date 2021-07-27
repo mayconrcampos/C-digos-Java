e8499fc26e8a3a358ef76b4abe93ca54344287cd
mayconrcampos <maycon.campos@gmail.com>
2021-07-27 19:00:27 -0300

    Aula 04 - tipos primitivos e escopo de variáveis

---
+++ Livro.java
@@ -0,0 +1,21 @@
+public class Livro {
+    
+    double preco = 10.50;
+    int quantidade = 20;
+    char tipo = 's';
+
+    public static int DESCONTO = 20;
+
+    public void vender(double valor){
+        double novoValor = valor - (valor * DESCONTO) / 100;
+    }
+
+    public void metodoContar(){
+        int contador = 20;
+        contador = contador + 1;
+    }
+
+    public void imprimir(int contador){
+        System.out.println(contador);
+    }
+}
